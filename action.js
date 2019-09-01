0c0d0e911616eb98c7a4b5cbbec9df488d42c378
Diablo9946 <diabloch9946@gmail.com>
2019-09-01 16:47:46 +0200

    Scheleton made

---
+++ action.js
@@ -0,0 +1,12 @@
+// EX DI OGGI
+// NOME REPO= js-jq-carousel
+// DESCRIZIONE=
+// Ricreare il carousel (o slideshow se preferite) visto a lezione.
+// Quindi frecce  nex prev + pallini.
+// NOTE=
+// quindi una cartella del repo con il progetto,
+// se poi volete far altro, es. controlli, animazioni etc.
+// fate sempre cartella aggiuntiva con versione alternativa
+// Se avete tempo vi consiglio di partire da zero quindi tutto html + css +js.
+// Se non avete molto tempo potete partire da qui:
+// https://bitbucket.org/booleancareers/lc-ex-slider-layout/src/master/
