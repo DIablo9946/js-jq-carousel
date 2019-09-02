4db4afa0c74ab5ba385f8e998973c3c69d3443c1
Diablo9946 <diabloch9946@gmail.com>
2019-09-02 06:18:39 +0200

    Done and working perfectly.

--- action.js
+++ action.js
@@ -10,3 +10,35 @@
 // Se avete tempo vi consiglio di partire da zero quindi tutto html + css +js.
 // Se non avete molto tempo potete partire da qui:
 // https://bitbucket.org/booleancareers/lc-ex-slider-layout/src/master/
+
+$(document).ready(function(){
+
+$(".next").click(nextslide);
+$(".prev").click(prevslide);
+
+function nextslide(){
+
+  var activeitems = $(".images img.active, .nav .fas.active");
+
+
+if ($(".last").hasClass("active")) {
+  $(".last").removeClass("active");
+  $(".first").addClass("active");
+} else {
+  activeitems.removeClass("active").next().addClass("active");
+}
+};
+
+function prevslide(){
+  var activeitems = $(".images img.active, .nav .fas.active");
+
+  if ($(".first").hasClass("active")) {
+    $(".first").removeClass("active");
+    $(".last").addClass("active");
+  } else {
+    activeitems.removeClass("active").prev().addClass("active");
+  }
+}
+
+
+});
